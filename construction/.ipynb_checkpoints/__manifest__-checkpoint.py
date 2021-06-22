6bfb4f2082f6460e6695d9a547c23ff58712b9ef
Sohersa <gerencia@gruposohersa.com>
2021-06-22 02:18:38 +0000

    templates file comented and icon test for app icon

--- construction/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ construction/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "construction",
+    'name': "Construction",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        This constructuion app is used to manage the resourses involved in a construction project (clients, machinery, materials, projects and more)
     """,
 
     'author': "Grupo Sohersa",
@@ -25,8 +25,8 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/views.xml'
+        #'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
