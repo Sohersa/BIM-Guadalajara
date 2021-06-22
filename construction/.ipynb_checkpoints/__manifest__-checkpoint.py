7212a51f5d8e15e20c018173627f2b29899cd963
Sohersa <gerencia@gruposohersa.com>
2021-06-22 02:38:56 +0000

    image directories added to manifest

--- construction/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ construction/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -12,7 +12,10 @@
 
     'author': "Grupo Sohersa",
     'website': "http://www.yourcompany.com",
-
+    'images': [
+        'static/description/icon.png',
+        'static/src/img/icon.png'
+    ],
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
