8eea6e459a70ba1313877c7f3a2a2d68f91a140d
Sohersa <gerencia@gruposohersa.com>
2021-06-23 00:13:04 +0000

    views added to manifest

--- construction/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ construction/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -18,18 +18,35 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Services/construction',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+        'web',
+        'website',
+        'base'
+    ],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml'
-        #'views/templates.xml',
+        'views/views.xml',
+        'views/workforce_views.xml',
+        'views/projects_views.xml',
+        'views/catalogs_views.xml',
+        'views/clients_views.xml',
+        'views/materials_views.xml',
+        'views/machinery_views.xml',
+        'views/suppliers_views.xml',
+        'views/bim_views.xml',
+        'views/assets.xml',
     ],
+    
+    'qweb': [
+        'static/src/xml/*.xml'
+    ],
+    
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
