ab424e191856b5e77d340d64b15230cc89eff46b
Sohersa <gerencia@gruposohersa.com>
2021-08-25 01:17:56 +0000

    fix 12

--- courses/models/.ipynb_checkpoints/students-checkpoint.py
+++ courses/models/.ipynb_checkpoints/students-checkpoint.py
@@ -3,7 +3,6 @@
 from odoo import models, fields, api
 
 class students(models.Model):
-    _inherit="res.partner"
     _name = "courses.students"
     _description = "Students"
         
@@ -13,7 +12,7 @@ class students(models.Model):
     partner_phone = fields.Char(related='partner_id.phone', string="Teléfono", widget="phone", store="True")
     partner_email = fields.Char(related='partner_id.email', string="Correo Electrónico", widget="email", store="True")        
     partner_country = fields.Many2one('res.country', related='partner_id.country_id', string="País", store="True")
-    partner_city = fields.Many2one('res.city', related='partner_id.city_id', string="Ciudad", store="True")
+    partner_city = fields.Many2one('res.city', related='partner_id.city_id', string="Ciudad", store="True", domain=['country_id','=', partner_country])
     partner_company = fields.Many2one('res.partner', related='partner_id.perent_id', string="Empresa", store="True")
     partner_function = fields.Char(related='partner_id.function', string="Puesto de Trabajo", store="True")
     partner_title = fields.Many2one('res.partner', related='partner_id.title', string="Profesión", store="True")
