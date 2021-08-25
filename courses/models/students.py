b4305e98314ad9c5068b7f99850b623c7cf48235
Sohersa <gerencia@gruposohersa.com>
2021-08-25 01:24:50 +0000

    fix 13

--- courses/models/students.py
+++ courses/models/students.py
@@ -7,13 +7,13 @@ class students(models.Model):
     _description = "Students"
         
     #Partner fields related to student
-    partner_id = fields.Many2one('res.partner', string="Contacto", store="True")
+    partner_id = fields.Many2one('res.partner', string="Contacto")
     name = fields.Char(related='partner_id.name', string="Nombre", store="True")
     partner_phone = fields.Char(related='partner_id.phone', string="Teléfono", widget="phone", store="True")
     partner_email = fields.Char(related='partner_id.email', string="Correo Electrónico", widget="email", store="True")        
     partner_country = fields.Many2one('res.country', related='partner_id.country_id', string="País", store="True")
-    partner_city = fields.Many2one('res.city', related='partner_id.city_id', string="Ciudad", store="True", domain=['country_id','=', partner_country])
     partner_company = fields.Many2one('res.partner', related='partner_id.perent_id', string="Empresa", store="True")
+    partner_city = fields.Many2one('res.city', related='partner_id.city_id', string="Ciudad", store="True")
     partner_function = fields.Char(related='partner_id.function', string="Puesto de Trabajo", store="True")
     partner_title = fields.Many2one('res.partner', related='partner_id.title', string="Profesión", store="True")
     
