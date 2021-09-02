3c961f0c5258adb6d6f7fbd0efe3d3f458406689
dario19999 <dario_gow3@htmail.com>
2021-09-02 13:51:46 -0500

    generations form view finished fix

--- courses/models/students.py
+++ courses/models/students.py
@@ -13,11 +13,11 @@ class students(models.Model):
     name = fields.Char(string="Nombre", store="True")
     partner_phone = fields.Char(related='partner_id.phone', string="Teléfono", widget="phone", store="True")
     partner_email = fields.Char(related='partner_id.email', string="Correo Electrónico", widget="email", store="True")
-#     partner_country = fields.Many2one('res.country', related='partner_id.country_id', string="País", store="True")
-#     partner_company = fields.Many2one('res.partner', related='partner_id.parent_id', string="Empresa", store="True")
-#     partner_city = fields.Char(related='partner_id.city', string="Ciudad", store="True")
-#     partner_function = fields.Char(related='partner_id.function', string="Puesto de Trabajo", store="True")
-#     partner_title = fields.Many2one('res.partner', related='partner_id.title', string="Profesión", store="True")
+    partner_country = fields.Many2one('res.country', related='partner_id.country_id', string="País", store="True")
+    partner_company = fields.Many2one('res.partner', related='partner_id.parent_id', string="Empresa", store="True")
+    partner_city = fields.Char(related='partner_id.city', string="Ciudad", store="True")
+    partner_function = fields.Char(related='partner_id.function', string="Puesto de Trabajo", store="True")
+#    partner_title = fields.Many2one('res.partner', related='partner_id.title', string="Profesión", store="True")
     
     origin = fields.Selection([
         ('company', 'Empresa'),
