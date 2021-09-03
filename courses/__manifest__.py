2c817ccd03cf5b0a62df616127ebe6a0c58995f4
dario19999 <dario_gow3@htmail.com>
2021-09-03 11:31:29 -0500

    groups submenu fix 4

--- courses/__manifest__.py
+++ courses/__manifest__.py
@@ -28,7 +28,7 @@
         'views/courses.xml',
         'views/generations.xml',
         'views/students.xml',
-        'views/groups.xml',
+        #'views/groups.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
