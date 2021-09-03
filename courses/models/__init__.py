e376f5c5fca8e8c62f130ab6aed76026bbb92926
dario19999 <dario_gow3@htmail.com>
2021-09-03 11:07:53 -0500

    groups submenu fix 2

--- courses/models/__init__.py
+++ courses/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import courses, generations, students, groups
+from . import courses, generations, students
--- courses/models/students.py
+++ courses/models/students.py
@@ -38,3 +38,14 @@ class students(models.Model):
 
     diploma = fields.Binary("Suba su Archivo")
     #channel_ids = fields.Many2many('mail.channel', 'mail_channel_profile_partner', 'partner_id', 'channel_id', copy='False')
+
+class student_group(models.Model):
+    _name = "courses.students.groups"
+    _description = "Student Groups"
+
+    name = fields.Char(string="Nombre", store="True")
+    cant_students = fields.Integer(string="Alumnos")
+
+    partner_id = fields.Many2one('res.partner', string="Representante")
+    generation_id = fields.Many2one('courses.generations', string="Curso")
+    student_ids = fields.One2many('courses.students', 'group_id', string="Alumnos")
