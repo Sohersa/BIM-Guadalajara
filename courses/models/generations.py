85e4678feaa0a419237b1313ba90159ccd5ef1a9
dario19999 <dario_gow3@htmail.com>
2021-09-16 00:35:14 -0500

    coursesgen_ids tree view changed - gens agenda & status fields added - student teams form view fixed

--- courses/models/generations.py
+++ courses/models/generations.py
@@ -10,12 +10,18 @@ class Generation(models.Model):
 
     name = fields.Char()
 
+    #Estado/fase de curso
+    status = fields.Selection([('new', 'Nuevo'),
+                               ('in_process', 'En curso'),
+                               ('terminated', 'Finalizado'),
+                               ('canceled', 'Cancelado')])
+
     #Counter fields for every student status
-    students_count = fields.Integer(string="Alumnos", readonly="1")
-    in_course_count = fields.Integer(string="Cursando", readonly="1")
-    pending_project_total = fields.Integer(string="Proyecto Pendiente", readonly="1")
-    approved_count = fields.Integer(string="Aprobados", readonly="1")
-    failed_count = fields.Integer(string="Reprobados", readonly="1")
+    students_count = fields.Integer(string="Alumnos", readonly="1") #computado
+    in_course_count = fields.Integer(string="Cursando", readonly="1") #computado
+    pending_project_total = fields.Integer(string="Proyecto Pendiente", readonly="1") #computado
+    approved_count = fields.Integer(string="Aprobados", readonly="1") #computado
+    failed_count = fields.Integer(string="Reprobados", readonly="1") #computado
 
     #Generation category
     category = fields.Selection([('public', 'Público'),
@@ -28,7 +34,15 @@ class Generation(models.Model):
     #relational field to students model
     student_ids = fields.One2many('courses.students', 'generation_id')
     
-    notes = fields.Text()
     speaker = fields.Many2one('hr.employee', string="Ponente")
 
+    #Course duration variables
+    total_hours = fields.Float(string="Duración del Curso (Horas)") #computado
+    total_days = fields.Float(string="Duración del Curso (Dias)") #computado
+    total_weeks = fields.Float(string="Duración del Curso (Semanas)") #computado
+    start_date = fields.Datetime(string="Fecha de Inicio") #calendario
+
+
     team_ids = fields.One2many('courses.student_teams', 'generation_id')
+
+    notes = fields.Text()
